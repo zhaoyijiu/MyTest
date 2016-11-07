a490feac72e91121c558dd3281dfa7e0d2374f29
zf <999>
2016-11-07 17:26:41 +0800

    1

---
+++ MyApplication1/app/src/main/java/com/zf/myapplication1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zf.myapplication1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //6666shiyishis
+    }
+}
